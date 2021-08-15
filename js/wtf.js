92a0c23d3a6d2a6f5105ae32332068138af32f5c
Den <52295780+josserden@users.noreply.github.com>
2021-08-15 10:09:23 +0300

    add lesson-1

---
+++ js/wtf.js
@@ -0,0 +1,21 @@
+// todo wtf или рубрика 'что будет в консоли'
+
+5 + '2'; // "52"
+(5 + '2' == '5' + '2') == '52';
+
+5 - '2'; // 3
+(5 - '2' == 5 - 2) == 3;
+
+true + true; // 2
+(true + true == 1 + 1) == 2;
+
+// true + false             // 1
+// 12 / '6'; // 2
+// "number" + 15 + 3        // 'number153'
+// 15 + 3 + "number"        // '18number'
+// [1] > null               // true
+// "foo" + + "bar"          // 'fooNaN'
+// 'true' == true           // false
+// false == 'false'         // false
+// null == ''               // false
+// !!"false" == !!"true"    // true
