4f322f17bdcaaa39083c9551bec3293156c359c9
Den <52295780+josserden@users.noreply.github.com>
2021-09-26 11:42:31 +0300

    create markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+
+import { galleryItems } from './js/galleryItems';
+import { refs } from './js/refs';
+import { createGallery } from './js/createGallery';
+
+const { gallery, lightbox, overlay, lightboxImg, closeModalBtn } = refs;
+
+// gallery.innerHTML = galleryItems.map(item => createGallery(item)).join('');
+
+const markup = galleryItems.map(item => createGallery(item)).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  overlay: document.querySelector('.lightbox__overlay'),
+  lightboxImg: document.querySelector('.lightbox__image'),
+  closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+};
